767f599f2791215bc489df7cffe6ab0555188933
Royce Calvin <royce20002006@yahoo.com>
2024-02-02 00:07:44 -0600

    finished with ana

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -3,27 +3,22 @@ function anagrams(str1, str2) {
   if(str1.length !== str2.length) return false;
   let obj = {};
   let obj2 = {};
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i].includes(obj)) {
-      obj[str1[i]]++;
-    } else {
-      obj[str1[i]] = 1; 
-    }
+
+  for (char of str1) {
+    obj[char] = (obj[char] || 0) + 1
   }
 
-  for(let i = 0; i < str2.length; i++) {
-    obj2[str2[i]] = (obj[str2[i]] || 0) + 1
+  for(let char of str2) {
+    obj2[char] = (obj2[char] || 0) + 1
   }
 
-  let sorted = Object.keys(obj).sort();
-  let sorted2 = Object.keys(obj2).sort();
 
-  for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] !== sorted2[i]) return false;
-    
 
+  for (let key in obj) {
+    if (obj[key] !== obj2[key]) return false;
   }
   return true;
+    
   
   
   
