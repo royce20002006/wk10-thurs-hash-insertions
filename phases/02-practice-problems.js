ca83ce134e0380cd23f9589b874a50016250962f
Royce Calvin <royce20002006@yahoo.com>
2024-02-01 23:51:17 -0600

    sorted

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,5 +1,33 @@
+
 function anagrams(str1, str2) {
-  // Your code here 
+  if(str1.length !== str2.length) return false;
+  let obj = {};
+  let obj2 = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i].includes(obj)) {
+      obj[str1[i]]++;
+    } else {
+      obj[str1[i]] = 1; 
+    }
+  }
+
+  for(let i = 0; i < str2.length; i++) {
+    obj2[str2[i]] = (obj[str2[i]] || 0) + 1
+  }
+
+  let sorted = Object.keys(obj).sort();
+  let sorted2 = Object.keys(obj2).sort();
+
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] !== sorted2[i]) return false;
+    
+
+  }
+  return true;
+  
+  
+  
+
 }
 
 
